051352441cf8b6b73a694dffbc313b825f92eb09
Kevin Kwak <zdevbro@gmail.com>
2025-11-10 16:24:10 +0900

    Fix CORS for production

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
